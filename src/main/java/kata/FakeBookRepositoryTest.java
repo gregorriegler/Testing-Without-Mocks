f6f4b20bcf0cb9543c24b411f6f68fd685863a5c
Gregor Riegler <gregor.riegler@ebcont.com>
2022-02-05 21:46:13 +0100

    Delete src/main/java/kata directory

--- src/main/java/kata/FakeBookRepositoryTest.java
+++
@@ -1,9 +0,0 @@
-package kata;
-
-public class FakeBookRepositoryTest extends BookRepositoryContract {
-
-    @Override
-    protected BookRepository createRepository() {
-        return new FakeBookRepository();
-    }
-}
